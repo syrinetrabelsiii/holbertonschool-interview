fa2f76164d84a3016087c28bc341c3fa6809b48e
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-12-11 23:46:40 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""lockboxes."""
 def canUnlockAll(boxes):
+    """
+    you have n number of locked boxes in front of you. Each box is numbered sequentially 
+    from 0 to n - 1 and each box may contain keys to the other boxes. 
+    """
     boxOne = [0]
     for key in boxOne:
         for keyBox in boxes[key]:
