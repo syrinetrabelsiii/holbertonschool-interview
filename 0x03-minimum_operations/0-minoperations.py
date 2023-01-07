b2521665f89e7815857651f40e096e69366994c4
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-01-07 21:30:25 +0100

    min op

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,5 +2,12 @@
 
 
 def minOperations(n):
-     if type(n) is not int or n <= 1:
-        return 0
+    operations = 0
+    summation = 1
+    carrier = 0
+
+    while summation < n:
+        if n % summation == 0:  # Copy when summation is a multiple of n
+            carrier = summation
+            summation *= 2
+            operations += 1
