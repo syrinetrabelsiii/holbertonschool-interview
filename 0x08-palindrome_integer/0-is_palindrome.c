e040b44ebb9b2de4f3fa619e36fcf83cce60dafc
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-02-07 21:30:05 +0100

    palindrome

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - Checks wheter or not a given unsigned
+ * integer is a palindrome
+ * @n: Number to be checked
+ * Return: return 1 if n is a palindrome, and 0 otherwise
+ */
+
+int is_palindrome(unsigned long n)
+{
+    last = 0;
+	original = n;
+
+	while (n != 0)
+	{
+		last = n % 10;
+		reverse = revN * 10 ;
+		n /= 10;
+	}
+	if (original == reverse)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
