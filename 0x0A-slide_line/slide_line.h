abcaa9a987db0faab498a1f93179cabe544dd1f3
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2023-02-23 22:07:16 +0100

    main file

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,16 +1,14 @@
-#ifndef SLIDE_LINE_H
-#define SLIDE_LINE_H
+#ifndef SLIDE_H
+#define SLIDE_H
 
-/* INCLUDED LIBRARIES */
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/* DEFINITIONS */
-#define SLIDE_LEFT -5
-#define SLIDE_RIGHT 5
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
 
-/* FUNCTION PROTOTYPES */
 int slide_line(int *line, size_t size, int direction);
-void slide(int *line, int index, int placeholder);
+void slide_left(int *line, size_t size);
+void slide_right(int *line, size_t size);
 
 #endif
