346a4de83c56f1eed4454756939cd66e77a081d8
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2024-02-25 20:06:43 +0100

    merge sort

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -1,73 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
-
-void merge(int *array, size_t left, size_t mid, size_t right)
+/**
+ * merge - merges left and right arrays
+ * @array: pointer to array
+ * @size: size of the array
+ * @left: pointer to left array
+ * @right: pointer to right array
+ **/
+void merge(int *array, int *left, int *right, size_t size)
 {
-    size_t i, j, k;
-    size_t n1 = mid - left + 1;
-    size_t n2 = right - mid;
+	int i = 0, j = 0, k = 0;
+	int size_l, size_r;
 
-    int *left_array = malloc(n1 * sizeof(int));
-    int *right_array = malloc(n2 * sizeof(int));
-
-    if (!left_array || !right_array)
-    {
-        printf("Error: Memory allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    for (i = 0; i < n1; i++)
-        left_array[i] = array[left + i];
-    for (j = 0; j < n2; j++)
-        right_array[j] = array[mid + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = left;
-
-    while (i < n1 && j < n2)
-    {
-        if (left_array[i] <= right_array[j])
-        {
-            array[k] = left_array[i];
-            i++;
-        }
-        else
-        {
-            array[k] = right_array[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1)
-    {
-        array[k] = left_array[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2)
-    {
-        array[k] = right_array[j];
-        j++;
-        k++;
-    }
-
-    free(left_array);
-    free(right_array);
+	size_l = size / 2;
+	size_r = size - size_l;
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(left, size_l);
+	printf("[right]: ");
+	print_array(right, size_r);
+	while (i < size_l && j < size_r)
+	{
+		if (left[i] < right[j])
+			array[k++] = left[i++];
+		else
+			array[k++] = right[j++];
+	}
+	while (i < size_l)
+		array[k++] = left[i++];
+	while (j < size_r)
+		array[k++] = right[j++];
+	printf("[Done]: ");
+	print_array(array, size);
 }
-
+/**
+ * merge_sort - merge sort in place on an array of integers
+ * @array: pointer to array
+ * @size: size of the array
+ **/
 void merge_sort(int *array, size_t size)
 {
-    if (size < 2)
-        return;
+	size_t middle = 0, i;
+	int left[MAX], right[MAX];
 
-    size_t mid;
-    mid = size / 2;
-
-    merge_sort(array, mid);
-    merge_sort(array + mid, size - mid);
-    merge(array, 0, mid - 1, size - 1);
+	if (array == NULL || size < 2)
+		return;
+	middle = size / 2;
+	for (i = 0; i < middle; i++)
+		left[i] = array[i];
+	for (i = middle; i < size; i++)
+		right[i - middle] = array[i];
+	merge_sort(left, middle);
+	merge_sort(right, size - middle);
+	merge(array, left, right, size);
 }
