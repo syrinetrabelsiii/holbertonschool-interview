5ccba39ec0709b75d647c8d142585f1d32fa2869
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2024-02-25 20:06:57 +0100

    sort

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -1,10 +1,10 @@
-#ifndef SORT_H
-#define SORT_H
-
+#ifndef _SORT_H
+#define _SORT_H
+#define MAX 100
 #include <stdlib.h>
+#include <stdio.h>
 
 void print_array(const int *array, size_t size);
-
 void merge_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif /* _SORT_H_ */
