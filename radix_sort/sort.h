2937fc98dbd1e70842c7cbda613352294de83613
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2024-03-12 23:08:06 +0100

    header file

--- radix_sort/sort.h
+++ radix_sort/sort.h
@@ -1,6 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
